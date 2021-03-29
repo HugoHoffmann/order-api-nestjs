53e94ffb14eaa13911ebfff9909f682c845108b0
Hugo Hoffmann <hugohoffmann04@gmail.com>
2021-03-28 23:40:26 -0300

    Initial commit

---
+++ src/modules/database/migrations/20210328232826_order.ts
@@ -0,0 +1,16 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('Order', table => {
+    table.increments('id').primary();
+    table.string('description', 50).notNullable();
+    table.integer('amount').notNullable();
+    table.decimal('value').notNullable();
+    table.dateTime('createdDate').notNullable();
+    table.dateTime('updatedDate').notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('Order');
+}
