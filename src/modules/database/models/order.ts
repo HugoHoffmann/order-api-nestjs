513299b75213b562ce72405b917c62e788d6cee8
Hugo Hoffmann <hugohoffmann04@gmail.com>
2021-03-29 20:50:13 -0300

    new: model order

---
+++ src/modules/database/models/order.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Model } from 'objection';
+
+import { IOrder } from '../interfaces/order';
+
+export class Order extends Model implements IOrder {
+  @ApiProperty({ type: 'integer' })
+  public id: number;
+  @ApiProperty({ type: 'string' })
+  public description: string;
+  @ApiProperty({ type: 'number' })
+  public amount: number;
+  @ApiProperty({ type: 'decimal' })
+  public value: number;
+  @ApiProperty({ type: 'string', format: 'date-time' })
+  public createdDate: Date;
+  @ApiProperty({ type: 'string', format: 'date-time' })
+  public updatedDate: Date;
+  public static get tableName(): string {
+    return 'Order';
+  }
+
+  public $beforeInsert(): void {
+    this.createdDate = this.updatedDate = new Date();
+  }
+
+  public $beforeUpdate(): void {
+    this.updatedDate = new Date();
+  }
+}
