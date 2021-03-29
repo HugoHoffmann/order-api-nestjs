7f355d437170807b75dbeb42a6d5b6c553877106
Hugo Hoffmann <hugohoffmann04@gmail.com>
2021-03-29 20:50:56 -0300

    new: seed order

--- src/modules/database/migrations/seeds/3-order.ts
+++ src/modules/database/migrations/seeds/3-order.ts
@@ -1,15 +1,21 @@
 import * as Knex from 'knex';
+import { IOrder } from 'modules/database/interfaces/order';
 
-export async function seed(knex: Knex): Promise<any> {
-  // Deletes ALL existing entries
-  return knex('table_name')
-    .del()
-    .then(() => {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        { id: 1, colName: 'rowValue1' },
-        { id: 2, colName: 'rowValue2' },
-        { id: 3, colName: 'rowValue3' }
-      ]);
-    });
+export async function seed(knex: Knex): Promise<void> {
+  const order: IOrder = {
+    description: 'Sushi',
+    amount: 5,
+    value: 1.25,
+    createdDate: new Date(),
+    updatedDate: new Date()
+  };
+
+  const users = await knex
+    .count()
+    .from('Order')
+    .first();
+
+  if (Number(users.count) > 0) return;
+
+  await knex.insert(order).into('Order');
 }
